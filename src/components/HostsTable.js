8301717de9fed8ce82c2d78f2824baba9b953997
Tim Brandsma <tbrandsma@republicwireless.com>
2020-09-25 11:29:30 -0400

    adding icons for active/inactive/retired hosts

--- src/components/HostsTable.js
+++ src/components/HostsTable.js
@@ -8,11 +8,12 @@ import {
   TableHead,
   TableRow,
   Paper,
-  Avatar
+  Avatar,
 } from '@material-ui/core';
+import { CloudOff, Check, Error, Help } from '@material-ui/icons';
 import { pure } from 'recompose';
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
     width: '100%',
     marginTop: theme.spacing(3),
@@ -23,7 +24,7 @@ const styles = theme => ({
   },
 });
 
-const HostsTable = pure(props => {
+const HostsTable = pure((props) => {
   const { classes, hosts, flanIps } = props;
 
   const flanEventsStyle = {
@@ -35,6 +36,7 @@ const HostsTable = pure(props => {
       <Table className={classes.table}>
         <TableHead>
           <TableRow>
+            <TableCell>Active</TableCell>
             <TableCell>Host Name</TableCell>
             <TableCell>Host ID</TableCell>
             <TableCell>Host Group</TableCell>
@@ -43,33 +45,60 @@ const HostsTable = pure(props => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {hosts.map(host => {
+          {hosts.map((host) => {
+            let activeIcon = null;
+
+            switch (host.active) {
+              case 'active':
+                activeIcon = <Check style={{ fill: 'green' }} />;
+                break;
+              case 'inactive':
+                activeIcon = <Error style={{ fill: 'red' }} />;
+                break;
+              case 'retired':
+                activeIcon = <CloudOff />;
+                break;
+              default:
+                activeIcon = <Help />;
+            }
             let flanEventCount = 0;
             let openAppCount = 0;
             let flanEvent = '';
             if (host.name in flanIps[0]) {
-              flanIps[0][host.name].forEach(app => {
+              flanIps[0][host.name].forEach((app) => {
                 flanEventCount += app['vulns'].length;
                 openAppCount += 1;
               });
             }
-            if(flanEventCount > 0) {
-              flanEvent = <Avatar aria-label="recipe" style={flanEventsStyle}>
+            if (flanEventCount > 0) {
+              flanEvent = (
+                <Avatar aria-label="recipe" style={flanEventsStyle}>
                   {flanEventCount}
                 </Avatar>
+              );
             }
             return (
               <TableRow
                 key={host.id}
                 hover
-                title={"Click to view host: " + host.name}
-                onClick={event => {
+                title={'Click to view host: ' + host.name}
+                onClick={(event) => {
                   props.history.push('/host/' + host.id);
                 }}
               >
+                <TableCell>{activeIcon}</TableCell>
                 <TableCell>{host.name}</TableCell>
                 <TableCell>{host.id}</TableCell>
-                <TableCell title={"Click to view group: " + host.group} Style="text-decoration:underline; cursor: pointer;" onClick={event => {event.stopPropagation(); props.history.push('/groupByName/' + host.group)}}>{host.group}</TableCell>
+                <TableCell
+                  title={'Click to view group: ' + host.group}
+                  Style="text-decoration:underline; cursor: pointer;"
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    props.history.push('/groupByName/' + host.group);
+                  }}
+                >
+                  {host.group}
+                </TableCell>
                 <TableCell>{flanEvent}</TableCell>
                 <TableCell>{openAppCount}</TableCell>
               </TableRow>
