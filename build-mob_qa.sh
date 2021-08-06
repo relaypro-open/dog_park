ff8939927ddb1f142425a4aa66dc755268e64212
Drew Gulino <dgulino@republicwirless.com>
2021-08-06 20:44:24 +0000

    changed url for mob_qa env

--- build-mob_qa.sh
+++ build-mob_qa.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-REACT_APP_DOG_API_ENV="qa" REACT_APP_DOG_API_HOST='https://dog.republicdev.info' yarn build
+REACT_APP_DOG_API_ENV="qa" REACT_APP_DOG_API_HOST='https://dog-qa.relaydev.sh' yarn build
