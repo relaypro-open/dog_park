59e418350f4eadd6f00d74ff083ddad5283491c5
Drew Gulino <dgulino@relaypro.com>
2021-08-06 17:07:52 -0400

    changed url for mob_pro env

--- build-mob_pro.sh
+++ build-mob_pro.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-REACT_APP_DOG_API_ENV="pro" REACT_APP_DOG_API_HOST='https://dog.republicwireless.com' yarn build
+REACT_APP_DOG_API_ENV="pro" REACT_APP_DOG_API_HOST='https://dog-pro.relaydev.sh' yarn build
